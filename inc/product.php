1a14251a71a618750e74b8fcc916fc36dc19da85
Bledioni <bledionmehmeti4@gmail.com>
2025-08-13 12:29:27 +0200

    Fixed ui of product

--- inc/product.php
+++ inc/product.php
@@ -60,78 +60,71 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../display/css/product.css">
 </head>
 <body>
-
 <?php require_once '../inc/dashBoradNavInc.php'; ?>
+
+<div class="product-page-wrapper">
 
     <div class="product-main-container">
         <div class="product-container">
+            <img class="product-main-image" src="../uploads/<?= htmlspecialchars($product['image_path']) ?>" alt="">
+            <div class="product-description">
+                <h1><?= htmlspecialchars($product['name']) ?></h1>
+                <p style="font-weight:600;"><?= htmlspecialchars($product['price']) ?>$</p>
+                <p><?= htmlspecialchars($product['description']) ?></p>
 
-        <img class="product-main-image" src="../uploads/<?= htmlspecialchars($product['image_path']) ?>" alt="">
+                <form action="" method="POST">
+                    <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
+                    <input type="number" name="quantity" min="1" required>
+                    <button type="submit">Add to Cart</button>
+                </form>
 
-        <div class="product-description">
-            <h1><?= htmlspecialchars($product['name']) ?></h1>
-            <p style="font-weight:600;" ><?= htmlspecialchars($product['price']) ?>$</p>
-            <p><?= htmlspecialchars($product['description']) ?></p>
+                <form action="./wishlistLogic.php" method="POST">
+                    <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
+                    <button type="submit" style="background:none; border:none;">
+                        <i id="wishlist" class="fa-solid fa-heart"></i>
+                    </button>
+                </form>
 
-            <form action="" method="POST">
-                <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
-                <input type="number" name="quantity" min="1" required>
-                <button type="submit">Add to Cart</button>
-            </form>
-
-            <form action="./wishlistLogic.php" method="POST">
-                <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
-                <button type="submit" style="background:none ; border: none;">
-                    <i id="wishlist" class="fa-solid fa-heart"></i>
-                </button>
-            </form>
-
-            <div class="futures">
-                <div class="delivery">
-                    <i class="fa-solid fa-truck-fast"></i>
-                    <div class="delivery-details">
-                        <h4>Free Delivery</h4>
-                        <p>Enter your postal code for Delivery Availability</p>
+                <div class="futures">
+                    <div class="delivery">
+                        <i class="fa-solid fa-truck-fast"></i>
+                        <div class="delivery-details">
+                            <h4>Free Delivery</h4>
+                            <p>Enter your postal code for Delivery Availability</p>
+                        </div>
                     </div>
-                </div>
-                <div class="delivery">
-                    <i class="fa-solid fa-repeat"></i>
-                    <div class="delivery-details">
-                        <h4>Return Delivery</h4>
-                        <p>Free 30 Days Delivery Returns. Details</p>
+                    <div class="delivery">
+                        <i class="fa-solid fa-repeat"></i>
+                        <div class="delivery-details">
+                            <h4>Return Delivery</h4>
+                            <p>Free 30 Days Delivery Returns. Details</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
-    </div>
     </div>
 
     <div id="todays-header">
-        <div id="todaysBox">
-        
-        </div>
-        <div id="todays-text">
-           Related Items
-        </div>
+        <div id="todaysBox"></div>
+        <div id="todays-text">Related Items</div>
     </div>
 
-
     <div class="related-products-container">
-    <?php foreach ($categories as $category): ?>
-        <?php if ($category['product_id'] !== $product['product_id'] && $category['category'] === $product['category']): ?>
-            <a href="../inc/product.php?product_id=<?= $category['product_id'] ?>">
-            <div class="related-product">
-                <img src="../uploads/<?= htmlspecialchars($category['image_path']) ?>" alt="">
-                <p class="name"><?= htmlspecialchars($category['name']) ?></p>
-                <p class="price"><?= htmlspecialchars($category['price']) ?>$</p>
-            </div>
-            <a href=""></a>
-        <?php endif; ?>
-    <?php endforeach; ?>
+        <?php foreach ($categories as $category): ?>
+            <?php if ($category['product_id'] !== $product['product_id'] && $category['category'] === $product['category']): ?>
+                <a href="../inc/product.php?product_id=<?= $category['product_id'] ?>">
+                    <div class="related-product">
+                        <img src="../uploads/<?= htmlspecialchars($category['image_path']) ?>" alt="">
+                        <p class="name"><?= htmlspecialchars($category['name']) ?></p>
+                        <p class="price"><?= htmlspecialchars($category['price']) ?>$</p>
+                    </div>
+                </a>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
 
-</div>
+</div> <!-- end wrapper -->
+
 <?= include_once '../inc/footer.php' ?>
-</a>
 </body>
-</html>
